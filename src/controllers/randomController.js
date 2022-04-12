0284e9736d21e4c29c039ec4be3ae075192ec482
jay123 <jay175147@gmail.com>
2022-04-12 21:27:49 +0530

    book

--- src/controllers/randomController.js
+++ src/controllers/randomController.js
@@ -1,9 +1,24 @@
-let addToArray= function (req, res) {
-    let x= req.body.number
-    console.log(x)
-    let arr= [2, 5, 11, 14]
-    arr.push(x)
-    res.send( {  msg: "post req 3", data: arr  } )
-}
+const mongoose=require('mongoose');
+// const { required } = require('nodemon/lib/config');
 
-module.exports.addToArray= addToArray
+const bookschema=new mongoose.Schema({
+bookname:{
+    type: String,
+    required: true,
+ },
+authorname: String, 
+category: 
+{
+    type:String,
+    enum:["science","maths","english","hindi"]
+},
+year: Number,
+date: {
+    type:Date,
+    default:Date.now,
+},
+ispublished:Boolean,
+
+},{timestamps:true})
+
+module.exports=mongoose.model('user',bookschema)
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://jaysh_db:LsPKC8Fxm42z2LaS@cluster0.g2uni.mongodb.net/jaysh_db",{
+    useNewUrlparser:true
+})
+.then( () => console.log("mongodb is connected"))
+.catch( err => console.log(err))
+
 app.use('/', route);
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
